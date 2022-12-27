04541fb9633c31031942b239142af45805021653
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2022-12-27 08:05:17 +0530

    エラーメッセージが重複しないように修正？

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates :password, format: { with: VALID_PASSWORD_REGEX }
+
 validates :nickname, presence: true
-validates :first_name, presence: true
-validates :last_name, presence: true
-validates :first_name_kana, presence: true
-validates :last_name_kana, presence: true
-validates :birthday, presence: true
+validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
+validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
+validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true }
+validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true }
+validates :birthday, presence: true, allow_blank: true
 
 
   has_many :items
