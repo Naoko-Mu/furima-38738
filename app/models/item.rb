d4373ca9d9c4cedf2f60ff10fd616eba452067fc
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2022-12-29 11:45:38 +0530

    商品の状態の選択を設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
 belongs_to :category
-
+belongs_to :conditin
 
 validates :image,         presence: true
 validates :item_name, presence: true
