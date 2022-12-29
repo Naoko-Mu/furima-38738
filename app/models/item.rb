d08e576b72ce2f371024a9cc986075f7feeb647e
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2022-12-29 16:44:51 +0530

    ¥300~¥9,999,999の間のみ・半角数値のみ保存可能の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ belongs_to :date_of_shipment
 
 validates :image,         presence: true
 validates :item_name, presence: true
-validates :price, presence: true
+validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: {with: /\A[0-9]+\z/}
 validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
 validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
 validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank"}
