0e65766d3a0ee3460ffb9b9eeee23ce411f516bf
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2022-12-29 11:36:18 +0530

    カテゴリー選択の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,21 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :category
+
+
 validates :image,         presence: true
 validates :item_name, presence: true
 validates :price, presence: true
-validates :category_id, presence: true
+validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
 validates :condition_id, presence: true
 validates :shipping_cost_id, presence: true
 validates :state_id, presence: true
 validates :date_of_shipment_id, presence: true
 validates :description, presence: true
 
+
 belongs_to :user
+
+
 end
