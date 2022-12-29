ad72a3854408b4c8d04ccda2dd31086d77d96251
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2022-12-29 16:19:02 +0530

    データの保存ができるように修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
 belongs_to :category
-belongs_to :conditin
+belongs_to :condition
 belongs_to :shipping_cost
 belongs_to :state
 belongs_to :date_of_shipment
@@ -11,10 +11,10 @@ validates :image,         presence: true
 validates :item_name, presence: true
 validates :price, presence: true
 validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
-validates :condition_id, presence: true
-validates :shipping_cost_id, presence: true
-validates :state_id, presence: true
-validates :date_of_shipment_id, presence: true
+validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
+validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank"}
+validates :state_id, numericality: { other_than: 1, message: "can't be blank"}
+validates :date_of_shipment_id, numericality: { other_than: 1, message: "can't be blank"}
 validates :description, presence: true
 
 
