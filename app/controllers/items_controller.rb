eaf55073b6ad7dc0b476e40b51e0d0952b393d63
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2023-01-06 15:18:01 +0900

    アクションの設定と削除ボタンの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -40,7 +40,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-
+    @item.destroy
+    redirect_to root_path if current_user.id == @item.user.id
   end
 
   private
