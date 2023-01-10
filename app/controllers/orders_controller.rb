cb8d9d04318c409cb2eac41a834bf2d51612e945
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2023-01-10 12:52:22 +0900

    購入画面ページに商品情報が反映されるように設定

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:zip_code, :state_id, :city, :street_address, :building_name, :phone_number, :user_id, :order_id, :item_id).merge(user_id: current_user.id)
+  end
+
+end
