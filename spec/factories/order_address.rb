bf629a6ef296de751c3672d7f83a3ec0f5b063f1
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2023-01-11 13:07:34 +0900

    Fromオブジェクト実装後のテストコードと制約の修正

---
+++ spec/factories/order_address.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    zip_code { '123-4567' }
+    state_id { 2 }
+    city { 'さいたま市' }
+    street_address { '1-1-1' }
+    building_name { 'しまじろう' }
+    phone_number { '09012345678' }
+  end
+end
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+ describe '購入情報の保存' do
+     before do
+      item = FactoryBot.create(:item)
+      user = FactoryBot.create(:user)
+       @order_address = FactoryBot.build(:order_address, item_id: item.id, user_id: user.id)
+       sleep(1)
+     end
+
+     context '購入情報が保存される場合' do
+       it 'すべての値が正しく入力されていれば保存できる' do
+         expect(@order_address).to be_valid
+       end
+       it 'building_nameは空でも保存できる' do
+         @order_address.building_name = ''
+         expect(@order_address).to be_valid
+       end
+     end
+
+     context '購入情報が保存されない場合' do
+       it 'zip_codeが空では保存されない' do
+         @order_address.zip_code = ''
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include("Zip code can't be blank")
+       end
+       it 'zip_codeにハイフンが含まれないと保存されない' do
+         @order_address.zip_code = '1234567'
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include('Zip code is invalid. Include hyphen(-)')
+       end
+       it 'zip_codeが全角だと保存されない' do
+         @order_address.zip_code = '１２３-４５６７'
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include('Zip code is invalid. Include hyphen(-)')
+       end
+       it 'state_idが1だと保存されない' do
+         @order_address.state_id = '1'
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include("State can't be blank")
+       end
+       it 'cityが空だと保存されない' do
+         @order_address.city = ''
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include("City can't be blank")
+       end
+       it 'street_addressが空だと保存されない' do
+         @order_address.street_address = ''
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include("Street address can't be blank")
+       end
+       it 'phone_numberが空だと保存されない' do
+         @order_address.phone_number = ''
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+       end
+       it 'phone_numberにハイフンが含まれると保存されない' do
+         @order_address.phone_number = '090-1234-5678'
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+       end
+       it 'phone_numberが全角では保存されない' do
+         @order_address.phone_number = '０９０１２３４５６７８'
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+       end
+       it 'phone_numberが9桁以下では保存されない' do
+         @order_address.phone_number = '090123456'
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+       end
+       it 'phone_numberが12桁以上では保存されない' do
+         @order_address.phone_number = '090123456789'
+         @order_address.valid?
+         expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+       end
+     end
+   end
+end
