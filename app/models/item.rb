40826047fff6b98e492d537ddaffc1aa9e1d4d70
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2023-01-12 15:52:44 +0900

    Rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,10 @@ class Item < ApplicationRecord
   has_one :order
 
   with_options presence: true do
-  validates :image
-  validates :item_name
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :description
+    validates :image
+    validates :item_name
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :description
   end
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,14 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-    with_options presence: true do
-
-  validates :nickname
-  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
-  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true }
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true }
-  validates :birthday
-    end
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true }
+    validates :birthday
+  end
 
   has_many :items
   has_many :orders
